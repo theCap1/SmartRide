9fc735eb4d7f0ce7f2496041925b73fb22499ca1
Sven Padutsch <sven.padutsch@sap.com>
2017-06-29 22:20:35 +0000

    Setup with Carousel at the top

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller",
-		"sap/ui/model/json/JSONModel",
-		"sap/m/MessageToast"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
 	],
 
 	function(Controller, JSONModel, Toast) {
